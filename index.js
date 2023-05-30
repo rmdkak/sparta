3e4255a2a83b28d8513793abb800418378fa7a5b
rmdkak <rmdkak99786@naver.com>
2023-05-30 14:58:19 +0900

    Second Commit
    Now we can get the information of movie with TMDB API

--- index.js
+++ index.js
@@ -13,33 +13,33 @@ fetch(
 )
   .then((response) => response.json())
   .then((response) => {
-    console.log(response);
+    // console.log(response);
     let result = response["results"];
-    console.log("result =>", result);
-    document.querySelectorAll(".movie_card").remove();
+    // console.log("result", result);
     result.forEach((a) => {
+      let m_id = a["id"];
       let m_title = a["title"];
       let m_overview = a["overview"];
       let m_poster_path = "https://image.tmdb.org/t/p/w500" + a["poster_path"];
       let m_vote_average = a["vote_average"];
+      console.log(m_id);
       console.log(m_title);
       console.log(m_overview);
       console.log(m_poster_path);
       console.log(m_vote_average);
-      let element = document.querySelector(".movie_list");
-      let temp_html = `<div class="movie_card2">
-                          <img
-                          src='${m_poster_path}'
-                          alt='${m_title}'
-                          />
-                          <h3>${m_title}</h3>
-
-                          <p>
-                          ${m_overview}
-                          </p>
-                          <p>${m_vote_average}</p>
-                        </div>`;
-      element.insertAdjacentHTML("beforeend", temp_html);
+      let card = document.querySelector(".card-list");
+      let temp_html = `<div class="movie-card" id="${m_id}">
+                      <img
+                        src="${m_poster_path}"
+                        alt="${m_title}"
+                      />
+                      <h3>${m_title}</h3>
+                      <p>
+                        ${m_overview}
+                      </p>
+                      <p>Rating: ${m_vote_average}</p>
+                    </div>`;
+      card.insertAdjacentHTML("beforeend", temp_html);
     });
   })
   .catch((err) => console.error(err));
