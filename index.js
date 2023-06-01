9c5f8ca93adef0292ae62e1ada2ecb9c75188049
rmdkak <rmdkak99786@naver.com>
2023-06-01 15:27:42 +0900

    FINAL
    필요 없는 내용은 삭제하고 못내 아쉬운 부분은 남겼습니다.
    차후에 사용할 수도 있을 것 같아서..

--- index.js
+++ index.js
@@ -11,18 +11,13 @@ fetch(
   "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
   options
 )
+  // TMDB API 카드 정보 구문
   .then((response) => response.json())
   .then((response) => {
     let result = response["results"];
     const newArray = result.map((e) => {
       const { id, title, overview, poster_path, vote_average } = e;
-      return {
-        id,
-        title,
-        overview,
-        poster_path,
-        vote_average,
-      };
+      return { id, title, overview, poster_path, vote_average };
     });
     newArray.forEach((a) => {
       let m_id = a["id"];
@@ -30,37 +25,52 @@ fetch(
       let m_overview = a["overview"];
       let m_poster_path = "https://image.tmdb.org/t/p/w500" + a["poster_path"];
       let m_vote_average = a["vote_average"];
-      let card = document.querySelector(".card-list"); // 템플릿 리터럴들을 담을 section을 class로 지정
+      let card = document.querySelector(".card-list");
       let temp_html = `<div class="movie-card" id="${m_id}" onclick="alert('id:'+ ${m_id})">                         
                           <img src="${m_poster_path}" alt="${m_title}"/>                         
                           <h3>${m_title}</h3>                   
                           <p>${m_overview}</p>
                           <p>Rating: ${m_vote_average}</p>
                        </div>`;
-      card.insertAdjacentHTML("beforeend", temp_html); // 해당 구역에 템플릿 리터럴들을 붙여줌
+      card.insertAdjacentHTML("beforeend", temp_html);
     });
   })
   .catch((err) => console.error(err));
 
+// 검색 구문
 function myFunction() {
-  // button에서 onclick 이벤트 발생 시 실행
   const input_val = document.getElementById("findMovie").value;
   if (input_val === "") {
-    alert("영화 제목을 한 글자 이상 입력해 주세요!"); // input에 value가 비어있을 때 alert 실행
+    alert("영화 제목을 한 글자 이상 입력해 주세요!");
   }
-  const input = document.getElementById("findMovie");
-  const keywords = input.value.toUpperCase(); // input에 입력된 value를 대문자로 치환
+  const keywords = input_val.toUpperCase();
   const movieList = document.getElementById("list");
-  const div = movieList.getElementsByTagName("div"); // section 안에 있는 태그 div(무비카드)들을 지칭
-  const myArray = [];
+  const div = movieList.getElementsByTagName("div");
   for (i = 0; i < div.length; i++) {
-    const title = div[i].getElementsByTagName("h3")[0].innerText; // 각 div마다의 h3태그.이너 텍스트 즉 타이틀 포함
-    myArray.push(title);
+    const title = div[i].getElementsByTagName("h3")[0].innerText;
     if (title.toUpperCase().indexOf(keywords) > -1) {
-      // textValue와 indexof(keywords)가 매칭 되는 결과가 있으면 그 값을 반환 그렇지않으면 -1을 반환
-      div[i].style.display = ""; // 매칭 결과가 반환되었을 경우 display 속성을 건들지 않음
+      div[i].style.display = "";
     } else {
-      div[i].style.display = "none"; // 매칭 결과를 제외한 값들 모두 display:none 처리
+      div[i].style.display = "none";
     }
   }
 }
+// 결국 안쓰게 됨(미관상)
+// let index = 0; //이미지에 접근하는 인덱스
+// window.onload = function () {
+//   slideShow();
+// };
+
+// function slideShow() {
+//   let i;
+//   let x = document.getElementsByClassName("slide1"); //slide1 참조
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none"; //처음에 전부 display를 none으로 함
+//   }
+//   index++;
+//   if (index > x.length) {
+//     index = 1; //인덱스가 초과되면 1로 변경
+//   }
+//   x[index - 1].style.display = "block"; //해당 인덱스는 block으로
+//   setTimeout(slideShow, 4000); //함수를 4초마다 호출
+// }
